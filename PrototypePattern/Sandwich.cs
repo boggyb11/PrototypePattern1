ebf6ff3dffb133671c6b8020da7f4fa2c9a8be86
Benjamin Fitzpatrick <Benjamin_Fitzpatrick@allfleetservices.co.uk>
2019-03-14 14:21:40 +0000

    first commit

---
+++ PrototypePattern/Sandwich.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+    class Sandwich : SandwichPrototype
+    {
+        private readonly string Bread;
+        private readonly string Meat;
+        private readonly string Cheese;
+        private readonly string Veggies;
+
+        public Sandwich(string bread, string meat, string cheese, string veggies)
+        {
+            Bread = bread;
+            Meat = meat;
+            Cheese = cheese;
+            Veggies = veggies;
+        }
+
+        public override SandwichPrototype Clone()
+        {
+            string ingredientList = GetIngredientList();
+            Console.WriteLine($"Cloning sandwich with ingredients: {ingredientList.Remove(ingredientList.LastIndexOf(","))}");
+
+            return MemberwiseClone() as SandwichPrototype;
+        }
+
+        private string GetIngredientList()
+        {
+            return $"{Bread}{Meat}{Cheese}{Veggies}";
+        }
+    }
+}
